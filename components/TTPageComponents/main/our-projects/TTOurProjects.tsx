9b0955529a7bd135ecffd188eac18b10ceb8d375
leontev.i.n <leontev.i.n@sblogistica.ru>
2024-05-25 16:48:14 +0300

    new comments

--- components/TTPageComponents/main/our-projects/TTOurProjects.tsx
+++ components/TTPageComponents/main/our-projects/TTOurProjects.tsx
@@ -5,13 +5,14 @@ import { OurProjects } from 'common/MainPage/OurProjects';
 import Image from 'next/image';
 
 import style from './TTOurProjects.module.scss';
+import Link from 'next/link';
 
 const { Text } = Typography;
 
 export const TTOurProjects = () => {
   const ourProjects = OurProjects();
   return (
-    <div style={{ padding: '0px 64px' }}>
+    <div className={`${style.wrapperOurProject}`} style={{ padding: '0px 64px' }}>
       <div style={{ marginBottom: 64 }}>
         <Heading2 label="Наши проекты" size={32} color="#232247" />
       </div>
@@ -30,14 +31,13 @@ export const TTOurProjects = () => {
                   <TTButton isColorBorder="blueFull" text="Получить консультацию" />
                 </Space>
               </Col>
-              <Col span={10}>
+              <Col span={10} style={{minHeight: 430}}>
                 <Image
                   src={item.imageUrl}
                   alt="Picture of the author"
                   // TODO: fix image size
-                  width={700}
-                  height={600}
-                  objectFit="contain"
+                  layout='fill'
+                  objectFit='contain'
                   // style={{ marginLeft: 22 }}
                 />
               </Col>
@@ -48,15 +48,19 @@ export const TTOurProjects = () => {
         return (
           <Row key={key + item.tag}>
             <Col span={2} />
-            <Col span={10} style={{ display: 'flex', justifyContent: 'flex-end', marginRight: 14, marginBottom: 104 }}>
+            <Col span={10}
+              style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    marginRight: 14,
+                    marginBottom: 104,
+                    minHeight: 430
+                  }}>
               <Image
                 src={item.imageUrl}
                 alt="Picture of the author"
-                // TODO: fix image size
-                width={700}
-                height={600}
-                objectFit="contain"
-                // style={{ marginRight: 32 }}
+                layout='fill'
+                objectFit='contain'
               />
             </Col>
             <Col span={10}>
@@ -73,6 +77,11 @@ export const TTOurProjects = () => {
           </Row>
         );
       })}
+      <Row>
+        <Col span={24}>
+          <Link href={'/doneproject'}>Все проекты</Link>
+        </Col>
+      </Row>
     </div>
   );
 };
